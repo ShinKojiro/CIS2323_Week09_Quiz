dd65f68dae7ef4e943b6eebd0c6d1865843d5787
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-10-17 18:22:11 -0500

    lotsa edits

--- Dice/Dice.java
+++ Dice/Dice.java
@@ -1,3 +1,4 @@
+package Dice;
 import java.util.*;
 
 public class Dice{
--- Play.java
+++ Play.java
@@ -1,13 +1,15 @@
 import java.util.*;
 import javax.swing.*;
 
+import Yahtzee.*;
+
 public class Play{
 	public static void main(String[] args){
 		Yahtzee game = new Yahtzee();
 		Scanner iDevice = new Scanner(System.in);
-		char select = '';
+		char select = 'z';
 		
-		while(select != 'D' || select != 'd'){
+		while(select != 'd'){
 			System.out.println("**************************\n" + 
 			"What do you want to do?\n" +
 			"A) Roll for Player one\n" +
@@ -21,15 +23,23 @@ public class Play{
 			iDevice.nextLine();
 			
 			if( select == 'a' || select == 'A'){
-			System.out.println(" Player one rolls ! \n" +
+			System.out.println(" Player One rolls ! \n" +
 			"   Player One rolled: \n" +
 			"       " + game.toss() + "\n" +
 			"**************************\n");
 			}
 			else if ( select == 'b' || select == 'B'){
-				
+			System.out.println(" Player Two rolls ! \n" +
+			"   Player Two rolled: \n" +
+			"       " + game.toss() + "\n" +
+			"**************************\n");
 			}
-			
+			else if ( select == 'c' || select == 'C'){
+				System.out.println("Player One Wins automatically !?");
+			}
+			//else if ( select == 'd' || select == 'D'){
+				
+			//}
 		}
 	}
 }
